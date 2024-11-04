9bb5b1a5c92c98cc3f3ad05ec039d6159f07df4a
Tim Prince <tim.prince@gmail.com>
2024-11-04 20:37:37 +0000

    Disable runout sensors since they aren't installed.

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1920,7 +1920,7 @@
  * RAMPS-based boards use SERVO3_PIN for the first runout sensor.
  * For other boards you may need to define FIL_RUNOUT_PIN, FIL_RUNOUT2_PIN, etc.
  */
-#define FILAMENT_RUNOUT_SENSOR // <-- changed
+//#define FILAMENT_RUNOUT_SENSOR // <-- changed
 #if ENABLED(FILAMENT_RUNOUT_SENSOR)
   #define FIL_RUNOUT_ENABLED_DEFAULT false // <-- changed: Enable the sensor on startup. Override with M412 followed by M500.
   #define NUM_RUNOUT_SENSORS   2          // <-- changed: Number of sensors, up to one per extruder. Define a FIL_RUNOUT#_PIN for each.
